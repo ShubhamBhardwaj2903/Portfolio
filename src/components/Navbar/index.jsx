196bcdb2ab0e791e13f3eabdfeaece88d8032068
Shubham <shubham.bhardwaj@somaiya.edu>
2024-05-12 23:43:04 +0530

    navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -29,57 +29,56 @@ const Navbar = () => {
     return (
         <div className="container">
             <div className={`navbar ${isOpen ? 'open' : ''}`} style={navbarStyle}>
-
                 <div className={`site-logo ${isOpen ? 'white' : ''}`} >Shubham</div>
                 <div className="menu-toggle">
-                <div className="menu-toggle-btn" onClick={toggleNavbar}>
+                    <div className="menu-toggle-btn" onClick={toggleNavbar}>
                         <img src={isOpen ? "/assets/cross.png" : "/assets/hamburger.png"} alt="Menu" />
                     </div>
                 </div>
             </div>
 
             <div className={`nav-container ${isOpen ? 'active' : ''}`}>
-                    <div className="nav">
-                        <div className="col flex">
-                            <div className="nav-logo">c/</div>
-                            <div className="nav-socials">
-                                <a href="#">LinkedIn</a>
-                                <a href="#">LinkedIn</a>
-                                <a href="#">LinkedIn</a>
-                                <a href="#">LinkedIn</a>
-                                <a href="#">LinkedIn</a>
-                            </div>
-                        </div>
-                        <div className="col">
-                            <div className="nav-link">
-                                <a href="#">Work</a>
-                            </div>
-                            <div className="nav-link">
-                                <a href="#">Services</a>
-                            </div>
-                            <div className="nav-link">
-                                <a href="#">About</a>
-                            </div>
-                            <div className="nav-link">
-                                <a href="#">Manifesto</a>
-                            </div>
-                            <div className="nav-link">
-                                <a href="#">Contact</a>
-                            </div>
+                <div className="nav">
+                    <div className="col flex">
+                        <div className="nav-logo">c/</div>
+                        <div className="nav-socials">
+                            <a href="#">LinkedIn</a>
+                            <a href="#">LinkedIn</a>
+                            <a href="#">LinkedIn</a>
+                            <a href="#">LinkedIn</a>
+                            <a href="#">LinkedIn</a>
                         </div>
                     </div>
-                    <div className="nav-footer">
-                        <div className="links">
-                            <a href="#">Privacy Policy</a>
-                            <a href="#">Cookie Policy</a>
-                            <a href="#">Terms & Condition</a>
+                    <div className="col">
+                        <div className="nav-link">
+                            <a href="#">Work</a>
                         </div>
-                        <div className="contact">
-                            <a href="#">bhardwajsb03@gmai.com</a>
+                        <div className="nav-link">
+                            <a href="#">Services</a>
+                        </div>
+                        <div className="nav-link">
+                            <a href="#">About</a>
+                        </div>
+                        <div className="nav-link">
+                            <a href="#">Manifesto</a>
+                        </div>
+                        <div className="nav-link">
+                            <a href="#">Contact</a>
                         </div>
                     </div>
                 </div>
-            
+                <div className="nav-footer">
+                    <div className="links">
+                        <a href="#">Privacy Policy</a>
+                        <a href="#">Cookie Policy</a>
+                        <a href="#">Terms & Condition</a>
+                    </div>
+                    <div className="contact">
+                        <a href="#">bhardwajsb03@gmai.com</a>
+                    </div>
+                </div>
+            </div>
+
         </div>
     );
 };
