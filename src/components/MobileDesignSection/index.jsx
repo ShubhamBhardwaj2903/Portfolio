330ae6b63f0f89edee59f06bdfaa53e701da67df
Shubham <shubham.bhardwaj@somaiya.edu>
2024-05-16 01:47:01 +0530

    design update

--- src/components/MobileDesignSection/index.jsx
+++ src/components/MobileDesignSection/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const MobileDesignSection = () => {
     return (
-        <div className='mobile design screen' style={{padding: 400}}>
+        <div className='mobile design' style={{padding: 400}}>
         </div>
     )
 }
