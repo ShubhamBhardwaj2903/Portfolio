70a27272383fa4b82b0d55f9cc57eeb0c174e980
Shubham <shubham.bhardwaj@somaiya.edu>
2024-05-17 02:50:40 +0530

    favicon

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './page/homePage';
-import Services from './components/Services';
-import IntroSection from './components/IntroSection'; 
-import ExperienceSection from './components/ExperienceSection';
-import ContactMe from './components/ContactMe'; 
 import Carzzy from './components/Carzzy/carzzy';
 import Ambli  from './components/Ambli/ambli'; 
 
@@ -13,11 +9,6 @@ const App = () => {
     <Router>
       <Routes>
         <Route exact path="/" element={<HomePage />} />
-        {/* <Route path="/services" element={<Services />} />
-        <Route path="/intro" element={<IntroSection />} />
-        <Route path="/experience" element={<ExperienceSection />} />
-        <Route path="/contact" element={<ContactMe />} /> */}
-
         <Route path="/carzzy" element={<Carzzy/>} />
         <Route path="/ambli" element={<Ambli />} />
       </Routes>
