b8a149539c459033d3c37562048b16ed299a8e96
Shubham <shubham.bhardwaj@somaiya.edu>
2024-05-16 23:44:03 +0530

    code update

--- src/components/MobileDesignSection/index.jsx
+++ src/components/MobileDesignSection/index.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
+// import React from 'react'
 
-const MobileDesignSection = () => {
-    return (
-        <div className='mobile design' style={{padding: 400}}>
-        </div>
-    )
-}
+// const MobileDesignSection = () => {
+//     return (
+//         <div className='mobile design' style={{padding: 400}}>
+//         </div>
+//     )
+// }
 
-export default MobileDesignSection
+// export default MobileDesignSection
